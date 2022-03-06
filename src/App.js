26202b4c44afb2df19674b847ad7f5813cdd99a4
Serge van den Oever <svdoever@gmail.com>
2022-03-06 20:03:10 +0100

    Optimize npm scripts, add call to api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [input, setInput] = useState('');
+    const [message, setMessage] = useState('');
+
+    const getMessage = async () => fetch(`/api/myHelloWorld?name=${encodeURIComponent(input)}`)
+    .then(response => response.text())
+    .then(text => setMessage(text));
+
+    useEffect(() => {
+        getMessage(input);
+    }, [input, getMessage])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <p>
+                    <label>Name:&nbsp;</label>
+                    <input value={input} onChange={evt => setInput(evt.target.value)} />
+                </p>
+                <p>
+                    <label>Message:&nbsp;</label>
+                    <span>{message}</span>
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
